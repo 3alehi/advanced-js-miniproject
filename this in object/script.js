3fefebadbbc705082ec82969703b2c5cbd790e98
slavik <116015277+masihSlavik@users.noreply.github.com>
2022-12-27 10:18:48 -0800

    Add files via upload

---
+++ this in object/script.js
@@ -0,0 +1,25 @@
+// const n = document.getElementById("name")
+// const l = document.getElementById("last")
+// const a = document.getElementById("age")
+// const c = document.getElementById("city")
+// let btn = document.querySelector('button')
+// // function userloge(name){
+// //   this.name = name
+// //   n.value = this.name
+// // }
+// // userloge.call(user,"slavik")
+// // console.log(user)
+// // function userloge(name){
+// //     this.name = name
+// //     n.value = this.name
+
+// // }
+// // userloge.apply(user,["ali"])
+// // console.log(user)
+
+// // function userloge(name){
+// //     this.name = name
+// // }
+
+// // userloge.bind(user,"amir")()
+// // console.log(user)
